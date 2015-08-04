44abae2092a873e7503bd65629ddbd72200a9b15
Will Holcomb <will@dhappy.org>
2015-08-04 17:21:57 -0400

    computed viewBox

--- app/components/area-svg.js
+++ app/components/area-svg.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 var AreaSvgComponent = Ember.Component.extend({
     tagName: 'svg',
-    attributeBindings: [ 'x', 'y', 'width', 'height' ]//,
-    //x: Ember.computed.alias('x')
+    attributeBindings: [ 'viewBox', 'x', 'y', 'width', 'height' ],
+    viewBox: Ember.computed.alias('area.attributes.viewBox')
 })
 
 export default AreaSvgComponent;
