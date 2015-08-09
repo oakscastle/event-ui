da7a07f733be470b19347b8ab8ae35fce2947223
Will Holcomb <will@dhappy.org>
2015-08-09 11:27:20 -0400

    trying for doc load in model

--- app/models/area.js
+++ app/models/area.js
@@ -5,7 +5,12 @@ var Area = DS.Model.extend({
     image_url: DS.attr('string'),
     parents: DS.hasMany('containers', { async: true }),
     attributes: DS.attr(),
-    contents: DS.attr('string')
+    contents: function() {
+        $.get(this.get('image_url')).then(function(data) {
+            return $('<g/>').append(data.documentElement.childNodes).html()
+            //area.set('attributes', { viewBox: data.documentElement.attributes.viewBox.value })
+        })
+    }.property('image_url')
 })
 
 export default Area
--- app/routes/area.js
+++ app/routes/area.js
@@ -1,12 +1,12 @@
 var AreaRoute = Ember.Route.extend({
-    afterModel: function(area) {
-        $.get(area.get('image_url')).then(function(data) {
-            area.set('contents', [$('<g/>').append(data.documentElement.childNodes).html()]) // parsing twice
+    // afterModel: function(area) {
+    //     $.get(area.get('image_url')).then(function(data) {
+    //         area.set('contents', [$('<g/>').append(data.documentElement.childNodes).html()]) // parsing twice
             
-            area.set('attributes', { viewBox: data.documentElement.attributes.viewBox.value })
-            //debugger
-        })
-    }
+    //         area.set('attributes', { viewBox: data.documentElement.attributes.viewBox.value })
+    //         //debugger
+    //     })
+    // }
 });
 
 export default AreaRoute;
